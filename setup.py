a8b67c6e86254321b520c9d61641655a6f8dfb13
Quentin <quentin.haenn@ensma.fr>
2024-11-13 13:04:03 +0100

    fix and close #1

--- setup.py
+++ setup.py
@@ -1,13 +1,29 @@
+import platform
 from setuptools import setup, Extension
 from Cython.Build import cythonize
 import numpy as np
+
+SYSTEM = platform.system()
+CPU = platform.processor()
+
+C_COMPILE_ARGS = ["-std=c99", "-O3", "-ffast-math", "-DREP"]
+CXX_COMPILE_ARGS = ["-std=c++11", "-O3", "-ffast-math"]
+CXX_LINK_ARGS = ["-std=c++11"]
+
+if not CPU:
+    CPU = platform.machine()
+
+if (SYSTEM != "Darwin") and (CPU not in 'arm64'):
+    C_COMPILE_ARGS.append("-march=native")
+    CXX_COMPILE_ARGS.append("-march=native")
+    CXX_LINK_ARGS.append("-fopenmp")
 
 extensions = [
     Extension(
         "radius_clustering.utils._emos",
         ["radius_clustering/utils/emos.pyx", "radius_clustering/utils/main-emos.c"],
         include_dirs=[np.get_include(), "radius_clustering/utils"],
-        extra_compile_args=["-std=c99", "-O3", "-march=native", "-ffast-math", "-DREP"],
+        extra_compile_args=C_COMPILE_ARGS,
     ),
     Extension(
         "radius_clustering.utils._mds_approx",
@@ -18,8 +34,8 @@ extensions = [
         ],
         include_dirs=[np.get_include(), "radius_clustering/utils"],
         language="c++",
-        extra_compile_args=["-std=c++11", "-O3", "-march=native", "-ffast-math"],
-        extra_link_args=["-std=c++11", "-fopenmp"],
+        extra_compile_args=CXX_COMPILE_ARGS,
+        extra_link_args=CXX_LINK_ARGS,
     ),
 ]
 
